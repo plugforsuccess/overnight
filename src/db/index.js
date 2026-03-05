80886853ba8e66a0b1e884d661ac9446fab36b3e
plugforsuccess <plugforsuccess@gmail.com>
2026-03-05 11:47:10 -0500

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,14 @@
-const knex = require('knex');
-const config = require('../../knexfile');
+// src/db/index.js
+const knex = require("knex");
+const { knexConfig } = require("./connection");
 
-const env = process.env.NODE_ENV || 'development';
-const db = knex(config[env]);
+let _db;
 
-module.exports = db;
+function getDb() {
+  if (_db) return _db;
+  _db = knex(knexConfig());
+  return _db;
+}
+
+module.exports = getDb();
+module.exports.getDb = getDb;
